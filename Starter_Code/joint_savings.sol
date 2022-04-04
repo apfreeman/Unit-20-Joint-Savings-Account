94e2534943f118ecc9c72f36e7644d672970921f
Adam Freeman <adam.freeman@outlook.com.au>
2022-04-04 20:32:12 +1000

    ver 0.2 - Draft(Compiled OK)

--- Starter_Code/joint_savings.sol
+++ Starter_Code/joint_savings.sol
@@ -43,12 +43,12 @@ contract JointSavings {
         /*
         Define a `require` statement that checks if the `recipient` is equal to either `accountOne` or `accountTwo`. The `required` statement returns the text `"You don't own this account!"` if it does not.
         */
-        require(recipient == accountOne || accountTwo, "You don't own this account!");
+        require(recipient == accountOne || recipient == accountTwo, "You don't own this account!");
 
         /*
         Define a `require` statement that checks if the `balance` is sufficient to accomplish the withdraw operation. If there are insufficient funds, the text `Insufficient funds!` is returned.
         */
-        require(balance >= amount, "Insufficient funds!");
+        require(address(this).balance >= amount, "Insufficient funds!");
 
         /*
         Add and `if` statement to check if the `lastToWithdraw` is not equal to (`!=`) to `recipient` If `lastToWithdraw` is not equal, then set it to the current value of `recipient`.
@@ -58,10 +58,10 @@ contract JointSavings {
         }
 
         // Call the `transfer` function of the `recipient` and pass it the `amount` to transfer as an argument.
-        msg.sender.transfer(msg.amount);
+        recipient.transfer(amount);
 
         // Set  `lastWithdrawAmount` equal to `amount`
-        lastWithdrawAmount = amount
+        lastWithdrawAmount = amount;
 
         // Call the `contractBalance` variable and set it equal to the balance of the contract by using `address(this).balance` to reflect the new balance of the contract.
         contractBalance = address(this).balance;
@@ -82,8 +82,8 @@ contract JointSavings {
     function setAccounts(address payable account1, address payable account2) public{
 
         // Set the values of `accountOne` and `accountTwo` to `account1` and `account2` respectively.
-        accountOne = account1
-        accountTwo = account2
+        accountOne = account1;
+        accountTwo = account2;
     }
 
     /*
